be1051391db16bad8f23ff478ddf642170ce4320
gitwunderbread <112037808+gitwunderbread@users.noreply.github.com>
2024-03-26 22:08:14 -0400

    Update index.js

--- index.js
+++ index.js
@@ -157,7 +157,7 @@ window.loadLeverJobs = function (options) {
 
       head.appendChild(link);
     }
-    addCss('https://andreasmb.github.io/lever-jobs-embed/embed-css/style.css');
+    addCss('https://gitwunderbread.github.io/style.css');
   }
 
   var request = new XMLHttpRequest();
