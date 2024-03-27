b36b903010bc999c17a59b1ad1421dbddef9c30a
gitwunderbread <112037808+gitwunderbread@users.noreply.github.com>
2024-03-27 01:20:42 -0400

    Update btn-index.js

--- btn-index.js
+++ btn-index.js
@@ -1,53 +1,70 @@
-url = 'https://api.lever.co/v0/postings/solestial?mode=json'
-
+url = "https://api.lever.co/v0/postings/solestial?mode=json";
 
 //Setting up the structure for each job posting
 function createJobs(_data) {
-
   //Checking for potential Lever source or origin parameters
   var pageUrl = window.location.href;
-  var leverParameter = '';
-  var trackingPrefix = '?lever-'
+  var leverParameter = "";
+  var trackingPrefix = "?lever-";
 
-  if( pageUrl.indexOf(trackingPrefix) >= 0){
+  if (pageUrl.indexOf(trackingPrefix) >= 0) {
     // Found Lever parameter
     var pageUrlSplit = pageUrl.split(trackingPrefix);
-    leverParameter = '?lever-'+pageUrlSplit[1];
+    leverParameter = "?lever-" + pageUrlSplit[1];
   }
   if (options.includeCss) {
     function addCss(fileName) {
       var head = document.head
-        , link = document.createElement('link');
+        , link = document.createElement("link");
 
-      link.type = 'text/css';
-      link.rel = 'stylesheet';
+      link.type = "text/css";
+      link.rel = "stylesheet";
       link.href = fileName;
 
       head.appendChild(link);
     }
-    addCss('https://gitwunderbread.github.io/btn-style.css');
+    addCss("https://gitwunderbread.github.io/btn-style.css");
   }
 
-  for(i = 0; i < _data.length; i++) {
-    var posting = _data[i]
-    var title = posting.text
-    var description = posting.description
+  for (i = 0; i < _data.length; i++) {
+    var posting = _data[i];
+    var title = posting.text;
+    var description = posting.description;
     //Making each job description shorter than 250 characters
-    var shortDescription = $.trim(description).substring(0, 150)
-    .replace('\n', ' ') + "...";
-    var location = posting.categories.location
-    var commitment = posting.categories.commitment
-    var team = posting.categories.team
-    var link = posting.hostedUrl+leverParameter
+    var shortDescription =
+      $.trim(description).substring(0, 250).replace("\n", " ") + "...";
+    var location = posting.categories.location;
+    var commitment = posting.categories.commitment;
+    var team = posting.categories.team;
+    var link = posting.hostedUrl + leverParameter;
 
-    $('#jobs-container .jobs-list').append(
-      '<div class="job '+team+' '+location+' '+commitment+'">' +
-        '<a class="job-title" href="'+link+'"">'+title+'</a>' +
-        '<p class="tags"><span>'+team+'</span><span>'+location+'</span><span>'+commitment+'</span></p>' +
-        '<p class="description">'+shortDescription+'</p>' +
-        '<a class="btn" href="'+link+'">Learn more</a>' +
-      '</div>'
-
+    $("#jobs-container .jobs-list").append(
+      '<div class="job ' +
+        team +
+        " " +
+        location +
+        " " +
+        commitment +
+        '">' +
+        '<a class="job-title" href="' +
+        link +
+        '"">' +
+        title +
+        "</a>" +
+        '<p class="tags"><span>' +
+        team +
+        "</span><span>" +
+        location +
+        "</span><span>" +
+        commitment +
+        "</span></p>" +
+        '<p class="description">' +
+        shortDescription +
+        "</p>" +
+        '<a class="btn" href="' +
+        link +
+        '">Learn more</a>' +
+        "</div>"
     );
   }
 }
@@ -56,6 +73,7 @@ function createJobs(_data) {
 $.ajax({
   dataType: "json",
   url: url,
-  success: function(data){
-    createJobs(data);}
+  success: function (data) {
+    createJobs(data);
+  }
 });
